0bdecc1e14e7b7bd59d12525c27d6a2f600f7e9c
liquidberny <bernardo.urquijo.bu@gmail.com>
2023-04-01 11:44:19 -0700

    Apuntes hechos del video, ahora se como se ven las variables

--- Tarea_MDD.m
+++ Tarea_MDD.m
@@ -21,17 +21,32 @@ Train_B=Train((Map_Benigno_T), :);
 A=unique(Train_M(:, 2:10));
 
 %Codigo para hacer conteo de Benignos
+
 %Sacamos los valores unicos de la matriz A:
 U = unique(A);
+
 %Contamos cuantos valores unicos tengo en A
 SU =size(U);
-%Inicializamos un vector
+
+%Inicializamos un vector de tamalo 10 x 10 iniciandolo en cero
 conteoBenigno=zeros(10,10);
+
+%Este es un for que va a ir de uno hasta el primer elemento del tamaño de
+%los unicos que haya tenido en A.
 for c=1:SU(1)
+    %vamos a sacar un mapa de cuantos elementos en Train_B son iguales a
+    %los elementos del vector unico
     mapBenigno=Train_B==U(c);
+    
+    %Como el mapa te va a dar ceros y unos le sumamos un uno para no
+    %manejar ceros
     conteoBenigno(c,:)=((sum(mapBenigno))+ 1);
 end
+
+%Al final de este for nos queda el vector con frecuencias de los Benignos
 conteoBenigno;
+
+%Concatenamos en una matriz
 TablaConteoBenigno=[U,conteoBenigno]; 
 
 %Codigo para hacer conteo de Malignos
